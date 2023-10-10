40c4e15b737a287f61e68fca98cd455b19b4ce0b
unknown <rubikonspace@ukr.net>
2023-10-10 17:50:40 +0300

    ww

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,16 @@
 import Modal from 'react-modal';
 import { Overlay, ModalWin } from './Modal.styled';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//   },
+// };
 
 Modal.setAppElement('#root');
 
