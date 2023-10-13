13777726d44299c4f41dcbc3876a3f6d822927b2
unknown <rubikonspace@ukr.net>
2023-10-13 20:08:54 +0300

    6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -124,7 +124,15 @@ export class App extends Component {
           items={fetchedImages}
           onClickImage={this.handleImageClick}
         />
-
+        {loading && (
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="3"
+            animationDuration="0.5"
+            width="70"
+            visible={true}
+          />
+        )}
         {loadMore && <ButtonLoadMore onClick={this.handleLoadMore} />}
 
         {showModal && (
